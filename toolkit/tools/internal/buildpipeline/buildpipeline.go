f250a3b0008db2a129a026aa73c19a87178435be
Lishan Liu <lishan.liu@intel.com>
2025-09-01 10:10:55 +0000

    Update Prod repo list

--- toolkit/tools/internal/buildpipeline/buildpipeline.go
+++ toolkit/tools/internal/buildpipeline/buildpipeline.go
@@ -24,7 +24,7 @@ const (
 	rootBaseDirEnv        = "CHROOT_DIR"
 	chrootLock            = "chroot-pool.lock"
 	chrootUse             = "chroot-used"
-	systemdDetectVirtTool = "systemd-detect-virt"
+	systemdDetectVirtTool = "checkIfContainerDockerEnvFile" //"systemd-detect-virt"
 )
 
 var isRegularBuildCached *bool
