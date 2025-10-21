2de273cb600f7bc60e795780dae3e6059aeb371b
andy.peng <andypeng@pglgull002.png.intel.com>
2025-10-21 14:05:58 +0800

    add git submodule script

---
+++ toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# This script clean up any change to the Azure Linux git submodule directory.
+
+set -e
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+if [ -d "${REPO_ROOT}/azurelinux" ]; then
+    cd ${REPO_ROOT}/azurelinux
+    git clean -xfd
+    git reset --hard
+    git submodule update --init
+    cd ${REPO_ROOT}
+    git clean -xfd
+    git reset --hard
+    [ ! -d ./SPECS-SIGNED ] && cp -r azurelinux/SPECS-SIGNED ./ 
+    [ ! -d ./SPECS-EXTENDED ] && cp -r azurelinux/SPECS-EXTENDED ./
+    skip_list=("xpmem" "iser" "isert" "mlnx-ethtool" "mlnx-iproute2" "mlnx-nfsrdma" "mlnx-ofa_kernel" "mlnx-tools" "azurelinux-image-tools" "azurelinux-release" "azurelinux-repos" "azurelinux-rpm-macros")
+    for folder in azurelinux/SPECS/*; do
+        fbasename=$(basename "$folder")
+        if [[ " ${skip_list[@]} " == *" ${fbasename} "* ]]; then
+            echo "Skipping $fbasename"
+            continue
+        fi
+        [ -d "$folder" ] && [ ! -d "SPECS/$fbasename" ] && cp -r "$folder" SPECS/ && echo "Copied $folder to SPECS/$fbasename"
+    done
+else
+    echo "Directory ${TIBEROS_DIR}/azurelinux does not exist. Skipping submodule update."
+fi
+echo "Submodules updated successfully"
