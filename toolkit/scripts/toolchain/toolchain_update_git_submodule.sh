6db657c377c96013d1508615e5d1ed1848b814ef
andy.peng <andypeng@pglgull002.png.intel.com>
2025-10-23 19:08:43 +0800

    update makefile

--- toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
+++ toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
@@ -10,9 +10,9 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 if [ -d "${REPO_ROOT}/azurelinux" ]; then
     cd ${REPO_ROOT}/azurelinux
-    git clean -xfd
-    git reset --hard
-    git submodule update --init
+    git clean -xfd &> /dev/null
+    git reset --hard &> /dev/null
+    git submodule update --init &> /dev/null
     cd ${REPO_ROOT}
     [ ! -d ./SPECS-SIGNED ] && cp -r azurelinux/SPECS-SIGNED ./ 
     [ ! -d ./SPECS-EXTENDED ] && cp -r azurelinux/SPECS-EXTENDED ./
@@ -20,12 +20,12 @@ if [ -d "${REPO_ROOT}/azurelinux" ]; then
     for folder in azurelinux/SPECS/*; do
         fbasename=$(basename "$folder")
         if [[ " ${skip_list[@]} " == *" ${fbasename} "* ]]; then
-            echo "Skipping $fbasename"
+            # echo "Skipping $fbasename"
             continue
         fi
-        [ -d "$folder" ] && [ ! -d "SPECS/$fbasename" ] && cp -r "$folder" SPECS/ && echo "Copied $folder to SPECS/$fbasename"
+        [ -d "$folder" ] && [ ! -d "SPECS/$fbasename" ] && cp -r "$folder" SPECS/ #&& echo "Copied $folder to SPECS/$fbasename"
     done
 else
-    echo "Directory ${TIBEROS_DIR}/azurelinux does not exist. Skipping submodule update."
+    # echo "Directory ${TIBEROS_DIR}/azurelinux does not exist. Skipping submodule update."
 fi
-echo "Submodules updated successfully"
+# echo "Submodules updated successfully"
