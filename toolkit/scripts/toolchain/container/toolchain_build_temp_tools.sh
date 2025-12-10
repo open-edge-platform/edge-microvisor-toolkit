2e3272d427f89f8b13cd8649e58397656ded4e14
Lishan Liu <lishan.liu@intel.com>
2025-12-10 12:50:15 +0000

    Update kernel to 6.12.59
    Update kernel to 6.12.59
    
    Signed-off-by: Lishan Liu <lishan.liu@intel.com>

--- toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
+++ toolkit/scripts/toolchain/container/toolchain_build_temp_tools.sh
@@ -86,9 +86,9 @@ rm -rf gcc-13.2.0
 
 touch $LFS/logs/temptoolchain/status_gcc_pass1_complete
 
-KERNEL_VERSION="6.12.55"
+KERNEL_VERSION="6.12.59"
 echo Linux-${KERNEL_VERSION} API Headers
-tar xf linux-6.12.55.tar.gz
+tar xf linux-6.12.59.tar.gz
 pushd linux-${KERNEL_VERSION}
 make mrproper
 make headers
