4ab0d519ec7527fe01fcb3752fc8572f9dcb0e37
andy-vm <108446482+andy-vm@users.noreply.github.com>
2025-12-29 11:51:23 +0800

    Git submodule  (#646)
    * add azurelinux
    
    * remove specs
    
    * remove specs
    
    * restore gcc
    
    * add glibc
    
    * add golang
    
    * add Cython
    
    * update check manifests
    
    * update license map
    
    * remove Cython
    
    * update license map
    
    * update Makefile
    
    * update script
    
    * update license map
    
    * update check license map
    
    ---------
    
    Co-authored-by: andy.peng <andypeng@pglgull002.png.intel.com>

---
+++ toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# This script clean up any change to the Azure Linux git submodule directory.
+
+set -e
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+if [ -d "${REPO_ROOT}/azurelinux" ]; then
+    cd ${REPO_ROOT}/azurelinux
+    git clean -xfd &> /dev/null
+    git reset --hard &> /dev/null
+    git submodule update --init &> /dev/null
+    cd ${REPO_ROOT}
+    skip_list=("cloud-provider-kubevirt" "dcos-cli" "vitess" "fwctl" "mlx-bootctl" "kernel-mshv" "srp" "mft_kernel" "knem" "xpmem" "xpmem-lib" "iser" "isert" "mlnx-ethtool" "mlnx-iproute2" "mlnx-nfsrdma" "mlnx-ofa_kernel" "mlnx-tools" "azurelinux-release" "azurelinux-repos" "azurelinux-rpm-macros")
+    for folder in azurelinux/SPECS/*; do
+        fbasename=$(basename "$folder")
+        skip=false
+        for skip_item in "${skip_list[@]}"; do
+            if [ "$fbasename" == "$skip_item" ]; then
+                skip=true
+                break
+            fi
+        done
+        if $skip; then
+            # echo "Skipping $fbasename"
+            continue
+        fi
+        [ -d "$folder" ] && [ ! -d "SPECS/$fbasename" ] && cp -r "$folder" SPECS/
+    done
+fi
