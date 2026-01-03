0edbfcd42f48a2642766dd3f8dbe1f5c0d820341
Lee Chee Yang <chee.yang.lee@intel.com>
2026-01-03 16:17:40 +0800

    update git script
    Signed-off-by: Lee Chee Yang <chee.yang.lee@intel.com>

--- toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
+++ toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
@@ -8,13 +8,46 @@ set -e
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
+skip_list=(
+    "apparmor"
+    "azurelinux-release"
+    "azurelinux-repos"
+    "azurelinux-rpm-macros"
+    "cloud-provider-kubevirt"
+    "dcos-cli"
+    "fwctl"
+    "fwctl-hwe"
+    "iser"
+    "iser-hwe"
+    "isert"
+    "isert-hwe"
+    "kernel-mshv"
+    "knem"
+    "knem-hwe"
+    "mft_kernel"
+    "mft_kernel-hwe"
+    "mlnx-ethtool"
+    "mlnx-iproute2"
+    "mlnx-nfsrdma"
+    "mlnx-nfsrdma-hwe"
+    "mlnx-ofa_kernel"
+    "mlnx-ofa_kernel-hwe"
+    "mlnx-tools"
+    "mlx-bootctl"
+    "srp"
+    "srp-hwe"
+    "vitess"
+    "xpmem"
+    "xpmem-hwe"
+    "xpmem-lib"
+)
+
 if [ -d "${REPO_ROOT}/azurelinux" ]; then
     cd ${REPO_ROOT}/azurelinux
     git clean -xfd &> /dev/null
     git reset --hard &> /dev/null
     git submodule update --init &> /dev/null
     cd ${REPO_ROOT}
-    skip_list=("cloud-provider-kubevirt" "dcos-cli" "vitess" "fwctl" "mlx-bootctl" "kernel-mshv" "srp" "mft_kernel" "knem" "xpmem" "xpmem-lib" "iser" "isert" "mlnx-ethtool" "mlnx-iproute2" "mlnx-nfsrdma" "mlnx-ofa_kernel" "mlnx-tools" "azurelinux-release" "azurelinux-repos" "azurelinux-rpm-macros")
     for folder in azurelinux/SPECS/*; do
         fbasename=$(basename "$folder")
         skip=false
