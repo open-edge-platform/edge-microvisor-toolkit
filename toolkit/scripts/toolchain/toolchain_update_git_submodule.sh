4ff7a6da9d75874cdb9c52d2f78a96d47f948e22
Lee Chee Yang <chee.yang.lee@intel.com>
2026-01-03 16:17:40 +0800

    update git script
    Signed-off-by: Lee Chee Yang <chee.yang.lee@intel.com>

--- toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
+++ toolkit/scripts/toolchain/toolchain_update_git_submodule.sh
@@ -8,6 +8,40 @@ set -e
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
+skip_list=(
+    "apparmor"
+    "azurelinux-release"
+    "azurelinux-repos"
+    "azurelinux-rpm-macros"
+    "cloud-provider-kubevirt"
+    "dcos-cli"
+    "fwctl"
+    "fwctl-hwe"
+    "iser"
+    "iser-hwe"
+    "isert"
+    "isert-hwe"
+    "kernel-mshv"
+    "knem"
+    "knem-hwe"
+    "mft_kernel"
+    "mft_kernel-hwe"
+    "mlnx-ethtool"
+    "mlnx-iproute2"
+    "mlnx-nfsrdma"
+    "mlnx-nfsrdma-hwe"
+    "mlnx-ofa_kernel"
+    "mlnx-ofa_kernel-hwe"
+    "mlnx-tools"
+    "mlx-bootctl"
+    "srp"
+    "srp-hwe"
+    "vitess"
+    "xpmem"
+    "xpmem-hwe"
+    "xpmem-lib"
+)
+
 if [ -d "${REPO_ROOT}/azurelinux" ]; then
     cd ${REPO_ROOT}/azurelinux
     git clean -xfd &> /dev/null
